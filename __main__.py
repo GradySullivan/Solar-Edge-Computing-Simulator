eb2c4634d5689500e3e84fab53f92bbaac9fbd36
GradySullivan <grady.sullivan18@gmail.com>
2021-10-07 00:01:45 -0400

    added policy file to store policy related functions

--- __main__.py
+++ __main__.py
@@ -3,6 +3,7 @@ import random
 from itertools import combinations
 from geopy.distance import geodesic as gd
 from setup import *
+from policies import *
 
 
 def get_applications_running(edge_dictionary):
@@ -49,8 +50,7 @@ if __name__ == '__main__':
         for pair in combos:
             loc1 = (pair[0].lat, pair[0].long)  # coordinates for location 1
             loc2 = (pair[1].lat, pair[1].long)  # coordinates for location 2
-            location_distances[pair] = gd(loc1, loc2)  # calculate distance between locations, add to dictionary
-    print(location_distances)
+            location_distances[pair] = gd(loc1, loc2)  # calculate distance between locations in km, add to dictionary
 
     applications = generate_applications(trace_info)  # generate list of application instances
 
@@ -61,73 +61,17 @@ if __name__ == '__main__':
     # ------------------ simulation ----------------
 
     processing_time = -1  # counter to tally simulation time (-1 indicates not started yet)
+    partially_completed_applications = {}  # apps that started running, but were shut down due to power constraints
 
-    while len(applications) != 0 or all_servers_empty is False:
+    while len(applications) != 0 or len(partially_completed_applications) or all_servers_empty is False:
 
         processing_time += 1
         print(f'Time = {processing_time}')
-        processing = []
 
-        # determine which servers are on
-        for edge in edge_computing_systems.keys():  # start by turning all servers back on
-            for server in edge.servers:
-                server.on = True
-        server_power_updated = False
-
-        # turn off servers w/o enough power (priority to keep servers on that are closest to completing a task)
-        while server_power_updated is False:
-            for edge in edge_computing_systems.keys():
-                servers_on = num_servers
-                power = edge.get_power_generated(irradiance_list[processing_time])  # update power available to edges
-                if power == 0:  # turn off all servers if no power
-                    print('shutting down all servers')
-                    for server in edge.servers:
-                        server.on = False
-                    server_power_updated = True
-                elif power / servers_on < power_per_server:  # determine how to shut down sites
-                    application_progression = {}
-                    while power / servers_on < power_per_server and servers_on > 0:
-                        for server in edge.servers:
-                            if server.applications_running == {}:
-                                server.on = False
-                                servers_on -= 1
-                                break
-                            application_progression[server] = max(server.applications_running).time_left
-                        if application_progression != {}:
-                            min_server = max(application_progression, key=application_progression.get)
-                            min_server.on = False
-                            del application_progression[min_server]
-                            servers_on -= 1
-                        if servers_on == 0:
-                            print('shutting down all servers')
-                            for server in edge.servers:
-                                server.on = False
-                            break
-                    server_power_updated = True
-                else:
-                    server_power_updated = True
-
-        # completing applications
-        for edge in edge_computing_systems.keys():  # for each edge computing site...
-            for server in edge.servers:  # for each server in a particular edge site
-                if server.on is True:
-                    for application in list(server.applications_running.keys()):  # for each application running...
-                        if application not in processing:  # if the application wasn't added in this time iteration...
-                            application.time_left -= 1
-                            # print(application, 'Time Left', application.time_left)
-                            if application.time_left <= 0:
-                                server.stop_application(application)
-                            processing.append(application)  # to prevent application decrementing multiple times
-
-        # start applications
-        for edge in edge_computing_systems.keys():  # for each edge computing site...
-            for server in edge.servers:  # for each server in a particular edge site
-                if server.on is True:
-                    for application in list(applications):  # for each application that still needs to run
-                        if (application.memory <= server.memory) and (application.cores <= server.cores):
-                            server.start_application(application)
-                            applications.remove(application)  # remove from to-do list
-
+        shutdown_servers(edge_computing_systems, partially_completed_applications, num_servers, power_per_server,
+                         irradiance_list, processing_time)
+        complete_applications(edge_computing_systems)  # completing applications
+        start_applications(edge_computing_systems, applications)  # start new applications if resources available
         all_servers_empty = get_applications_running(edge_computing_systems)  # check if applications are running
 
     simplify_time(processing_time)  # simulation time
